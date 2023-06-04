cc81881bd29b6ab7fb4f82ed09aa37e7cd01956c
akasakakona <tomliang20011018@gmail.com>
2023-06-04 00:49:34 -0700

    Temporary checkpoint

--- CS122A_P2.ino
+++ CS122A_P2.ino
@@ -1,55 +1,122 @@
-#include <LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
 #include <string.h>
+#include "Queue.h"
+#include "Timer.h"
 
+struct task{
+    int state;
+    unsigned long period;
+    unsigned long elapsedTime;
+    int (*TickFct)(int);
+};
 
 int noiseCnt = 0;
-const int rs = 8, en = 7, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal LCD(rs, en, d4, d5, d6, d7);
+//LCD Address: 0x27, 16 columns, 2 rows
+LiquidCrystal_I2C LCD(0x27, 16, 2);
+const int MAX_OCCUPANCY = 5;
+bool isNoise = false;
+bool isOccupied = false;
+bool isMotion = false;
+Queue<int> msgQ;
+Queue<int> msgQRPi;
+
+enum MicSM_States {MicSM_Start, MicSM_Detection, MicSM_SetResult};
+enum MotionSM_States {MotionSM_Start, MotionSM_Detection, MotionSM_SetResult};
+
+void TickFct_MicSM(int MicSM_State){
+  static unsigned int maxNoise;
+  static unsigned int minNoise;
+  static unsigned i;
+  switch(MicSM_State){
+    case MicSM_Start:
+      MicSM_State = MicSM_Detection;
+      maxNoise = 0;
+      minNoise = 1024;
+      i = 0;
+      break;
+    case MicSM_Detection:
+      if(i >= 50){
+        MicSM_State = MicSM_SetResult;
+      }else{
+        MicSM_State = MicSM_Detection;
+      }
+      break;
+    case MicSM_SetResult:
+      MicSM_State = MicSM_Detection;
+      maxNoise = 0;
+      minNoise = 1024;
+      break;
+    default:
+      MicSM_State = MicSM_Start;
+      break;
+  }
+  switch(MicSM_State){
+    case MicSM_Detection:
+      unsigned int sample = analogRead(A0);
+      if(sample > maxNoise){
+        maxNoise = sample;
+      }else if(sample < minNoise){
+        minNoise = sample;
+      }
+      i++;
+      break;
+    case MicSM_SetResult:
+      unsigned int noise = ((maxNoise - minNoise) * 5) / 1024;
+      if(noise){
+        isNoise = true;
+      }
+      break;
+  }
+}
+
+void TickFct_MotionSM(int MotionSM_State){
+  switch(MotionSM_State){
+    case MotionSM_Start:
+      MotionSM_State = MotionSM_Detection;
+      break;
+    case MotionSM_Detection:
+      if(digitalRead(10) == HIGH){
+        isNoise = true;
+        MotionSM_State = MotionSM_SetResult;
+      }else{
+        MotionSM_State = MotionSM_Detection;
+      }
+      break;
+    case MotionSM_SetResult:
+      if(digitalRead(10) == LOW){
+        isNoise = false;
+        MotionSM_State = MotionSM_Detection;
+      }else{
+        MotionSM_State = MotionSM_SetResult;
+      }
+    default:
+      MotionSM_State = MotionSM_Start;
+      break;
+  }
+  switch(MotionSM_State){
+    case MotionSM_Detection:
+      break;
+    case MotionSM_SetResult:
+      break;
+    default:
+      break;
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(A0, INPUT);
-  pinMode(9, OUTPUT);
+  
+  Serial.begin(9600); //for serial debugging
+  pinMode(A0, INPUT); //for microphone
+  pinMode(9, OUTPUT); //for LED
+  pinMode(10, INPUT); //for PIR Motion Sensor
   digitalWrite(9, LOW);
   
-  LCD.begin(16, 2);
-  LCD.print("Noise Detected:");
-  LCD.setCursor(0, 1);
-  LCD.print(String(noiseCnt) + " times.");
+  LCD.begin(); //LCD Setup
+  LCD.setBacklight(HIGH);
+  LCD.setCursor(0, 0);
+  LCD.print("Hello World!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  static int timer = -1;
-  unsigned long start = millis();
-  unsigned int max = 0;
-  unsigned int min = 1024;
-  while(millis()-start < 50){
-    unsigned int sample = analogRead(A0);
-    if(sample > max){
-      max = sample;
-    }else if(sample < min){
-      min = sample;
-    }
-  }
-  unsigned int noise = ((max - min) * 5) / 1024;
-  if(noise){
-    noiseCnt++;
-    Serial.println("Noise Detected!");
-    digitalWrite(9, HIGH);
-    timer = 5000;
-    LCD.clear();
-    LCD.begin(16, 2);
-    LCD.print("Noise Detected:");
-    LCD.setCursor(0, 1);
-    LCD.print(String(noiseCnt) + " times.");
-  }
-  if(timer <= 0){
-    digitalWrite(9, LOW);
-    timer = 0;
-  }else{
-    timer -= millis() - start;
-  }
-  // Serial.println();
+
 }
---
+++ Queue.h
@@ -0,0 +1,52 @@
+template <typename T>
+class Queue {
+public:
+    Queue(){
+        front = nullptr;
+        back = nullptr;
+        length = 0;
+    }
+    ~Queue(){
+        clear();
+    }
+    void enqueue(T data){
+        if(isEmpty()){
+            front = new Node(data);
+            back = front;
+        }else{
+            back->next = new Node(data);
+            back = back->next;
+        }
+        length++;
+    }
+    T dequeue(){
+        if(isEmpty()){
+            return T();
+        }
+        Node* temp = front;
+        T data = temp->data;
+        front = front->next;
+        delete temp;
+        length--;
+        return data;
+    }
+    bool isEmpty(){
+        return length == 0;
+    }
+    int size(){
+        return length;
+    }
+    void clear(){
+        while(!isEmpty()){
+            dequeue();
+        }
+    }
+private:
+    struct Node {
+        T data;
+        Node* next;
+    };
+    Node* front;
+    Node* back;
+    int length;
+};
